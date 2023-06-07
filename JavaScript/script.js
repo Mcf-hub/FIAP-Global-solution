1f8159361e494dbf1c8dd051538b1539d476dda4
Arthur Bogiano <atacktody@gmail.com>
2023-06-07 17:30:01 -0300

    mini mudanças

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,28 +1,34 @@
 // Definindo o valor máximo do contador
 var maxCount = 828000000;
 
-// Obtendo o elemento HTML onde o contador será exibido
-var contadorElemento = document.getElementById("contador");
-
 // Função para iniciar o contador
 function iniciarContador() {
+
   // Definindo o valor inicial do contador
   var contador = 0;
 
-  // Definindo o intervalo de atualização do contador (a cada 1 milissegundo)
-  var intervalo = setInterval(function () {
-    // Atualizando o valor do contador
-    contador++;
+  // Obtendo o elemento HTML onde o contador será exibido
+  var contadorElemento = document.getElementById("contador");
 
-    // Exibindo o valor atualizado no elemento HTML
-    contadorElemento.textContent = contador.toLocaleString();
+  if (contadorElemento) {
 
-    // Verificando se o contador atingiu o valor máximo
-    if (contador === maxCount) {
-      // Parando o intervalo de atualização
-      clearInterval(intervalo);
-    }
-  }, 1);
+    // Definindo o intervalo de atualização do contador (a cada 1 milissegundo)
+    var intervalo = setInterval(function () {
+      // Atualizando o valor do contador
+      contador++;
+  
+      // Exibindo o valor atualizado no elemento HTML
+      contadorElemento.textContent = contador;
+  
+      // Verificando se o contador atingiu o valor máximo
+      if (contador === maxCount) {
+        // Parando o intervalo de atualização
+        clearInterval(intervalo);
+      }
+    }, 1);
+    
+  }
+
 }
 
 // Chamando a função para iniciar o contador quando a página terminar de carregar
